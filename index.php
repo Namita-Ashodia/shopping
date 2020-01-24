c193649f95d440d30297f6bc2f02c758649cdd11
Namita Ashodia <58926975+Namita-Ashodia@users.noreply.github.com>
2020-01-24 16:05:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+  
+  include 'navbar.php';
+
+?>
+<div class="container">    
+  <div class="row">
+    <div class="col-sm-2">
+    </div>
+    <div class="col-sm-8">
+    <div class="panel panel-primary"> 
+          
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="tm.jpg" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="tm1.jpg" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="tm2.jpg" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+      </div> 
+    </div>
+    <div class="col-sm-2"> 
+    </div>
+  </div>
+</div><br>
+
+<div class="container">    
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading" align="center">New Arrival For Men</div>
+        <div class="panel-body"><img src="t1.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer" align="center">Men's International Causal Wear</br><div align="center">50-70% off</div></div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-danger">
+        <div class="panel-heading" align="center">New Arrival For Women</div>
+        <div class="panel-body"><img src="t7.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer" align="center">Men's International Causal Wear</br><div align="center">50-70% off</div></div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-success">
+        <div class="panel-heading" align="center">Top Styles Under $99</div>
+        <div class="panel-body"><img src="t3.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer" align="center">Men's International Causal Wear</br><div align="center">50-70% off</div></div>
+      </div>
+    </div>
+  </div>
+</div><br>
+
+<div class="container">    
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading" align="center">Winter Collection</div>
+        <div class="panel-body"><img src="t10.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer" align="center">Men's International Causal Wear</br><div align="center">50-70% off</div></div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-primary">
+        <div class="panel-heading" align="center">Flat $5 off of on 1st Order</div>
+        <div class="panel-body"><img src="t5.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer" align="center">Men's International Causal Wear</br><div align="center">50-70% off</div></div>
+      </div>
+    </div>
+    <div class="col-sm-4"> 
+      <div class="panel panel-primary">
+        <div class="panel-heading" align="center">Spring Fest</div>
+        <div class="panel-body"><img src="t6.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer" align="center">Men's International Causal Wear</br><div align="center">50-70% off</div></div>
+      </div>
+    </div>
+  </div>
+</div><br><br>
+
+<?php include 'footer.php'; ?>
